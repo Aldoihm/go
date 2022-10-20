0d6b77e48dbb1a766674418452dabdc366f0152b
Aldo Predator <ingaldo@sotecmovil.com>
2022-10-19 22:37:47 -0500

    se crea carpeta comentarios y se enseñan los estandares en los comentarios de git

---
+++ comentarios/main.go
@@ -0,0 +1,27 @@
+// package main, es el paquete principal
+// que me permite ejecutar mi aplicación de
+// EDteam
+package main
+
+//Test es una variable de prueba
+var Test = 123
+
+//Pi es una constante que vale 3.14
+const Pi = 3.14
+
+//comentario en una línea
+/* Comentario multilinea*/
+
+//En Go se usa el comentario multilinea para código que no voy a usar y para todo lo demás
+//se usa el comentario de unsa sola línea
+//NOTA:para generar la documentación en go se usa
+/*	go doc --all*/
+//pero nos debemos colocar en la carpeta raíz de nuestro proyecto
+//para generar mi documentación desde github, ir a la pagina godoc.org.miruta
+//Example (godoc.org.github.com/Aldoihm/go)
+
+/*
+func main() {
+
+}
+*/
