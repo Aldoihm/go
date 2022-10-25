2dbfa3fb66f26dcfeb3057384d6f2a314974027e
Aldo Predator <ingaldo@sotecmovil.com>
2022-10-25 10:10:15 -0500

    switch, se crea la carpeta switch

---
+++ switch/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* PRIMERA FORMA
+	switch expresion {
+	case condition :
+
+	case condition :
+
+	default:
+	} */ //no es necesario la palabra reservada break
+
+	emoji := "🐺"
+	/* switch emoji {
+	case "🐈", "🐕", "🐺":
+		fmt.Println("es un animal")
+	case "🍌", "🍎":
+		fmt.Printf("es un fruta")
+	default:
+		fmt.Println("no es un animal ni una fruta")
+	} */
+
+	/* SEGUNDA FORMA:
+	switch  {
+	case operador_comparacion & operadores_logicos :
+
+	case == || == :
+
+	default:
+	}
+	*/
+
+	switch {
+	case emoji == "🐈" || emoji == "🐕" || emoji == "🐺":
+		fmt.Println("es un animal")
+	case emoji == "🍌" || emoji == "🍎":
+		fmt.Printf("es un fruta")
+	default:
+		fmt.Println("no es un animal ni una fruta")
+	}
+
+}
