b76ba64ba8e679a057dedbfa4e59f9787b16a52e
Aldo Predator <ingaldo@sotecmovil.com>
2022-10-26 21:24:02 -0500

    panic y recover, panico nos saca del programa y recover controla el panico y nos regresa al programa, se crea la carpeta panic. Revisar bien el codigo para entender las 2 funciones

---
+++ panic/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	//PANIC:nos permite entrar en panico, osea finalizar la ejecución cuando panic es llamada
+	//RECOVER: nos puede recuperar después de la generación de un pánico
+	division(10, 5)
+	division(9, 3)
+	division(1, 0)
+	division(8, 2)
+
+}
+func division(dividendo, divisor int) {
+	//aqui se ejecuta el recover
+	defer func() {
+		r := recover() //me devuelve un valor del panic
+		if r != nil {  //solo se ejecutara si hay un panic
+			fmt.Println("recuperandome del panic", r)
+		}
+	}()
+	validandoDivisor(divisor)
+	fmt.Println(dividendo / divisor)
+}
+func validandoDivisor(divisor int) {
+	if divisor == 0 {
+		panic("🤦") //Aqui se usa el panic
+	}
+}
