9495a48d6989320e98583c95207c418d88ef60b3
Aldo Predator <ingaldo@sotecmovil.com>
2022-10-29 10:45:19 -0500

    Se crea un nuevo modulo go.mod, se guarda en el repositorio de la nube

---
+++ paquetes/hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/Aldoihm/go
+
+go 1.19
--- paquetes/hello/main.go
+++ paquetes/hello/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
-	"greet"
 )
 
 func main() {
 	fmt.Println("Hello")
 
-	greet.English()
 }
+
+/* Cómo usar los paquetes que nosotros creamos o paquetes de terceros: con MODULOS
+Los Modulos nos ayudan administrar las dependencias de nuestros paquetes y controlar las versiones de estos paquetes
+
+Podemos tener diferentes módulos en nuestro repositorio
+Pero se recomienda que tengamos uno solo en la raíz de nuestro proyecto
+
+go mod init: crea un nuevo modulo de go
+Se recomienda alojarlo en la ruta donde se encuentra nuestro código fuente
+
+Ejemplo:
+go mod init github.com/Aldoihm/go
+
+Intenté varias maneras.. pero la de arriba es la correcta
+*/
