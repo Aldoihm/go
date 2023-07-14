d7412b2ad30d48eb9185d3a97943a9c64085fe90
Ing. Aldo <ingaldo@sotecmovil.com>
2023-07-14 06:23:17 -0600

    Se actualiza el tema de constantes al curso 2023, mucho más completo

--- constantes/main.go
+++ constantes/main.go
@@ -2,8 +2,35 @@ package main
 
 import "fmt"
 
+// Constantes a nivel de paquete y en forma de agrupaciones
+// Las constantes a diferencia de las variables, no necesitan ser usadas forzosamente en el programa para que compile
+const (
+	os     = "linux"
+	domain = "ed.team"
+)
+
+// Constante con iota para las secuencias, comenzando desde cero, ejemplo
+const (
+	Ene = iota + 1
+	Feb
+	Mar
+	Abr
+	May
+	Jun
+)
+
 func main() {
-	//en constante no se puede usar la creación de variable corta :=
-	const pi = 3.14
-	fmt.Println(pi)
+	/*1ra forma para declarar constantes
+	const os string = "linux"*/
+
+	/*Segunda forma para declarar constantes
+	const os, domain string = "linux", "ed.team"*/
+
+	//No se puede usar el operador de variable corta
+
+	/*Tercera forma
+	const os, domain = "linux", 1*/
+
+	fmt.Println(os, domain)
+	fmt.Println(May)
 }
