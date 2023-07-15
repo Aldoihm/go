f147afc1a6e55a0f9e3c33b9ddafd7e5e2284034
Ing. Aldo <ingaldo@sotecmovil.com>
2023-07-15 06:06:53 -0600

    Actualización de operadores en go: aritméticos, asignación, post-incrementro, post-decremento, comparación, lógicos y negación

--- operadores/main.go
+++ operadores/main.go
@@ -3,20 +3,30 @@ package main
 import "fmt"
 
 func main() {
+	//Operadores aritméticos: (), *, /, %, +, -
+	//Operadores de asignación: =, +=, -=, /=, %=
 	var a = 4 + 2*3
 	var b = 10
 	b += 2
 	fmt.Println(a)
 	fmt.Println(b)
+	//Declaraciones post-incremento y post-decremento: ++, --
+	//No son una expresión, sino una declaración
+	//Forma correcta
 	var c = 3
 	c--
+	//Forma incorrecta
+	/*
+		var c int = 5
+		c= c++
+	*/
 	fmt.Println(c)
-	//Operadores de Comparación: >, <, ==, !=, >=.<=
+	//Operadores de Comparación: >, <, ==, !=, >=.<=. Respuesta booleana
 	fmt.Println(b != a)
 	//Operadores Lógicos: &&,||
 	var age = 30
-	fmt.Println("age:", age >= 18 && age < 60)
-	fmt.Println("niño o viejo:", age < 18 || age >= 60)
-	//Unario : !
-	fmt.Println(!(4 != 4))
+	fmt.Println("Es adulto?:", age >= 18 && age < 60)
+	fmt.Println("Es niño o viejo?:", age < 18 || age >= 60)
+	//Unario : ! (negación)
+	fmt.Println(!(4 == 4))
 }
