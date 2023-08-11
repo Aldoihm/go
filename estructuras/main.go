46727ce3ad4ca7abe6bb6c8852a6de2fbbbc628f
Ing. Aldo <ingaldo@sotecmovil.com>
2023-08-11 08:35:18 -0600

    Actualización del tema de estructuras

--- estructuras/main.go
+++ estructuras/main.go
@@ -2,21 +2,23 @@ package main
 
 import "fmt"
 
-func main() {
-	//las estructuras dejan almacenar una colección de campos, como las clases en otros lenguajes de programación
-	type course struct {
-		Name     string
-		Profesor string
-		Country  string
-	}
+// las estructuras dejan almacenar una colección de campos, como las clases en otros lenguajes de programación
+// Por lo general las estructuras se declaran a nivel de paquete
+type Course struct {
+	Name     string
+	Profesor string
+	Country  string
+}
 
-	db := course{
+func main() {
+
+	db := Course{
 		Name:     "Bases de Datos",
 		Profesor: "Aldo",
 		Country:  "México",
 	}
-	git := course{"Git", "Beto", "Bolivia"}
-	css := course{Profesor: "Albaro"}            //si no asgino valor a los campos, go deja valor a 0 dependiendo del tipo de dato
+	git := Course{"Git", "Beto", "Bolivia"}
+	css := Course{Profesor: "Albaro"}            //si no asgino valor a los campos, go deja valor a 0 dependiendo del tipo de dato
 	fmt.Printf("Bases de datos: %+v\n", db.Name) //v= valor, +=nombre del campo
 	fmt.Printf("Git: %+v\n", git)
 	fmt.Printf("CSS: %+v\n", css)
@@ -28,6 +30,7 @@ func main() {
 	fmt.Printf("Puntero: %+v\n", p)
 	//cambiar valores desde el puntero
 	(*p).Profesor = "Alexis"
+	//Si se trabaja con estructuras y punteros, go permite no colocar la desreferenciación (*) ejemplo:
 	p.Country = "Colombia"
 	fmt.Printf("Base d Datos: %+v\n", db)
 }
