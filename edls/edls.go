f86ef93019d18696656f9e78357b4d39ad5057aa
Ing. Aldo <ingaldo@sotecmovil.com>
2023-09-21 08:50:55 -0600

    se continua con el proyecto edls, se agregan las estructuras y las flags

--- edls/edls.go
+++ edls/edls.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 // file types
 const (
 	fileRegular int = iota
@@ -22,3 +24,36 @@ const (
 	jpg = ".jpg"
 	gif = ".gif"
 )
+
+// File struct
+type file struct {
+	name             string
+	fileType         int
+	isDir            bool
+	isHidden         bool
+	userName         string
+	groupName        string
+	size             int64
+	modificationTime time.Time
+	mode             string
+}
+
+// Style File
+type styleFileType struct {
+	icon   string
+	color  string
+	symbol string
+}
+
+//Mapa como llave el tipo de archivo para retornar una estructura
+
+var mapStyleByFileType = map[int]styleFileType{
+	fileRegular:    {icon: "📜"},
+	fileDirectory:  {icon: "📂", color: "BLUE", symbol: "/"},
+	fileExecutable: {icon: "🚀", color: "GREEN", symbol: "*"},
+	fileCompress:   {icon: "📦", color: "RED"},
+	fileImage:      {icon: "📸", color: "MAGENTA"},
+	fileLink:       {icon: "🔗", color: "CYAN"},
+}
+
+//me quedé en el minuto 9:40
