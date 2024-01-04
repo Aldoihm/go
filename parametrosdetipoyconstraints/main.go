2140abddc5d98dad06296a85bae3881a682ca2f1
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-01-04 12:21:21 -0600

    Constraints a partir de interfaces y explicación de importacion de paquetes

--- parametrosdetipoyconstraints/main.go
+++ parametrosdetipoyconstraints/main.go
@@ -18,6 +18,9 @@ func main() {
 	fmt.Println(sum[MyInt](num3, 20))
 	fmt.Println(sum(2, 4.6))
 
+	fmt.Println("Restricciones a partir de interfaces")
+	fmt.Println(sumas(2.5, 4.6, -5.1))
+	fmt.Println(sumas(2.5, 4.6, float64(num3)))
 }
 
 //Constrains, en español significa restricciones y son para las funciones de any
@@ -28,6 +31,17 @@ func main() {
 1.- Arbitrario
 2.- De unión de elementos
 3.- Aproximación de elementos
+4.- A partir de interfaces
+5.- Importando paquetes externos
+	Usan interfaces como la que hicimos en el 4, pero ya declarados sin que nosotros los escribamos.
+	1.- go mod init XXXXX // Las XXXX => Es cualquier nombre que desees asignarle por lo general ponen "main"
+	2.- Luego: go mod tidy
+	3.-Y por último: go get golang.org/x/exp/constraints
+	Con eso se instalara el paquete
+
+	NOTA IMPORTANTE: con go mod init main tiene que ser por cada git raiz.
+	Si en un git tenemos varios programas de go, al ejecutar go mod init, generaremos problemas con los demás
+	programas por esta razon no hice el del paso 5.
 */
 
 // Las constraints arbitrarias son una pendejada inservible, por eso no las puse.
@@ -43,6 +57,19 @@ func suma[T int | float64](nums ...T) T {
 // Constraints de aproximación de elementos
 // se añade el simbolo ~ para indicar para cualquier tipo derivado de entero
 func sum[T ~int | float64](nums ...T) T {
+	var total T
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
+
+// Constraints a partir de interfaces
+type Numeric interface {
+	~int | ~uint | ~float32 | ~float64
+}
+
+func sumas[T Numeric](nums ...T) T {
 	var total T
 	for _, num := range nums {
 		total += num
